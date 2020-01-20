50f26151346e43f90efb27a4b80325ec486963df
Stephano Cetola <scetola@linuxfoundation.org>
2020-01-20 15:37:45 -0800

    make the temp sensor do something random
    Signed-off-by: Stephano Cetola <scetola@linuxfoundation.org>

--- temp_sensor.sv
+++ temp_sensor.sv
@@ -1,4 +1,18 @@
 `include "defs.sv"
 module temp_sensor(input logic clk, input logic reset, output logic tick, output logic[7:0] temp);
     
+    //seems silly, but they are synchronous.
+    assign tick = clk;
+
+    always @(posedge clk)
+    begin
+        if (reset)
+        begin
+            temp <= '0;
+        end
+        else
+        begin
+            temp <= $urandom_range(0,255);
+        end
+    end
 endmodule
